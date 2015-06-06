0e56fa5603b08825719f13c0921742dc13563c11
Kanibal <kanibalclonk@freenet.de>
2015-06-06 02:16:29 +0200

    * Refactored standard library code * updated StdBuf to support move semantics * tweaked cmake-files to support OSX

--- standard/src/Midi.cpp
+++ standard/src/Midi.cpp
@@ -9,40 +9,35 @@
 #include <midi.h>
 #include <stdio.h>
 
-BOOL PlayMidi(const char *sFileName, HWND appWnd)
-	{
+BOOL PlayMidi(const char *sFileName, HWND appWnd) {
   char buf[256];
-  sprintf(buf, "open \"%s\" type sequencer alias ITSMYMUSIC", sFileName);  
-	if (mciSendString("close all", NULL, 0, NULL) != 0)
-    return FALSE;  
-  if (mciSendString(buf, NULL, 0, NULL) != 0)
-    return FALSE;
+  sprintf(buf, "open \"%s\" type sequencer alias ITSMYMUSIC", sFileName);
+  if (mciSendString("close all", NULL, 0, NULL) != 0) return FALSE;
+  if (mciSendString(buf, NULL, 0, NULL) != 0) return FALSE;
   if (mciSendString("play ITSMYMUSIC from 0 notify", NULL, 0, appWnd) != 0)
-    return FALSE; 
+    return FALSE;
   return TRUE;
-	}
+}
 
-BOOL PauseMidi()
-	{
-	if (mciSendString("stop ITSMYMUSIC", NULL, 0, NULL) != 0) return FALSE;
-	return TRUE;
-	}
+BOOL PauseMidi() {
+  if (mciSendString("stop ITSMYMUSIC", NULL, 0, NULL) != 0) return FALSE;
+  return TRUE;
+}
 
-BOOL ResumeMidi(HWND appWnd)
-	{       
-	if (mciSendString("play ITSMYMUSIC notify", NULL, 0, appWnd) != 0) return FALSE;
-	return TRUE;
-	}
+BOOL ResumeMidi(HWND appWnd) {
+  if (mciSendString("play ITSMYMUSIC notify", NULL, 0, appWnd) != 0)
+    return FALSE;
+  return TRUE;
+}
 
-BOOL StopMidi()
-	{
-	if (mciSendString("close all", NULL, 0, NULL) != 0) return FALSE;
-	return TRUE;
-	}
+BOOL StopMidi() {
+  if (mciSendString("close all", NULL, 0, NULL) != 0) return FALSE;
+  return TRUE;
+}
 
-BOOL ReplayMidi(HWND appWnd)
-	{
-	if (mciSendString("play ITSMYMUSIC from 0 notify", NULL, 0, appWnd) != 0) return FALSE;
-	return TRUE;
-	}
-#endif //HAVE_MIDI_H
+BOOL ReplayMidi(HWND appWnd) {
+  if (mciSendString("play ITSMYMUSIC from 0 notify", NULL, 0, appWnd) != 0)
+    return FALSE;
+  return TRUE;
+}
+#endif  // HAVE_MIDI_H
--- standard/src/StdNoGfx.cpp
+++ standard/src/StdNoGfx.cpp
@@ -2,28 +2,24 @@
 #include <Standard.h>
 #include <StdNoGfx.h>
 
-BOOL CStdNoGfx::CreateDirectDraw()
-	{
-	Log("  Graphics disabled");
-	return TRUE;
-	}
+BOOL CStdNoGfx::CreateDirectDraw() {
+  Log("  Graphics disabled");
+  return TRUE;
+}
 
-CStdNoGfx::CStdNoGfx()
-	{
-	Default();
-	}
+CStdNoGfx::CStdNoGfx() { Default(); }
 
-CStdNoGfx::~CStdNoGfx()
-	{
-	delete lpPrimary; lpPrimary = NULL;
-	Clear();
-	}
+CStdNoGfx::~CStdNoGfx() {
+  delete lpPrimary;
+  lpPrimary = NULL;
+  Clear();
+}
 
-bool CStdNoGfx::CreatePrimarySurfaces(BOOL Fullscreen, int iColorDepth, unsigned int iMonitor)
-	{
-	// Save back color depth
-	byByteCnt = iColorDepth / 8;
-	// Create dummy surface
-	lpPrimary = lpBack = new CSurface();
-	return true;
-	}
+bool CStdNoGfx::CreatePrimarySurfaces(BOOL Fullscreen, int iColorDepth,
+                                      unsigned int iMonitor) {
+  // Save back color depth
+  byByteCnt = iColorDepth / 8;
+  // Create dummy surface
+  lpPrimary = lpBack = new CSurface();
+  return true;
+}
